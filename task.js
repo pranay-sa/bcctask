33d4a5ad0a5675ef46d4e752de6ecdf82a0c8149
Pranay <116664160+pranay-sa@users.noreply.github.com>
2024-03-15 20:33:55 +0530

    Add files via upload

--- task.js
+++ task.js
@@ -1,1 +1,13 @@
-'use strict';
+document.querySelector('.btnright').addEventListener('click',function(){
+    console.log("CLICKED");
+    const consentCheckbox = document.querySelector('input[name="consent"]');
+    if (consentCheckbox.checked) {
+        let tDate = new Date();
+        const tDay = tDate.getDate();
+        const tMonth = tDate.getMonth();
+        const tYear = tDate.getFullYear();
+        document.getElementById('dateFilled').textContent = `Form Filled On ${tDay}-${tMonth}-${tYear}`;
+    } else {
+        alert("check the consent checkbox");
+    }
+})
